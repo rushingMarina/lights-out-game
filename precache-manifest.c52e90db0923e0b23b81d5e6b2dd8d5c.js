dac7985725e62950d0783a8ec6064af32c63d99f
mordva42 <333marina2007@gmail.com>
2019-07-25 17:38:47 -0700

    Updates

--- precache-manifest.c52e90db0923e0b23b81d5e6b2dd8d5c.js
+++ precache-manifest.c52e90db0923e0b23b81d5e6b2dd8d5c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
   },
   {
-    "revision": "0e2e5300e00815ec89a4",
-    "url": "/lights-out-game/static/js/main.0e2e5300.chunk.js"
+    "revision": "abf49423fd766b781833",
+    "url": "/lights-out-game/static/js/main.abf49423.chunk.js"
   },
   {
     "revision": "e614b0c7a408a3da68bb",
     "url": "/lights-out-game/static/js/1.e614b0c7.chunk.js"
   },
   {
-    "revision": "0e2e5300e00815ec89a4",
-    "url": "/lights-out-game/static/css/main.b259b7ee.chunk.css"
+    "revision": "abf49423fd766b781833",
+    "url": "/lights-out-game/static/css/main.6e03fe36.chunk.css"
   },
   {
-    "revision": "c439b1ea540369e16f4c86cc86141053",
+    "revision": "0a725eb48bd27c4f937373cf91d7ec55",
     "url": "/lights-out-game/index.html"
   }
 ];
