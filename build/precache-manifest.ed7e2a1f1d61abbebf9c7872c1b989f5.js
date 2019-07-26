35c5de8709f75315df8169db7c477628abd7ea6a
mordva42 <333marina2007@gmail.com>
2019-07-25 17:37:12 -0700

    added responsivness

---
+++ build/precache-manifest.ed7e2a1f1d61abbebf9c7872c1b989f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "0e2e5300e00815ec89a4",
+    "url": "/lights-out-game/static/js/main.0e2e5300.chunk.js"
+  },
+  {
+    "revision": "e614b0c7a408a3da68bb",
+    "url": "/lights-out-game/static/js/1.e614b0c7.chunk.js"
+  },
+  {
+    "revision": "0e2e5300e00815ec89a4",
+    "url": "/lights-out-game/static/css/main.b259b7ee.chunk.css"
+  },
+  {
+    "revision": "c439b1ea540369e16f4c86cc86141053",
+    "url": "/lights-out-game/index.html"
+  }
+];
